43edb0979e6444ac2e722a5df457c30bacfdd291
Praveen <praveenkothapally18@gmail.com>
2024-08-15 00:00:53 +0530

    Advanced vpc module

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,18 @@
 #   value = module.vpc.vpc_id # module.<name>.<output-variable-name>
 # }
 
-output "azs" {
-  value = module.vpc.azs
+# output "azs" {
+#   value = module.vpc.azs
+# }
+
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = local.private_subnet_ids
+}
+
+output "database_subnet_ids" {
+  value = local.database_subnet_ids
 }
